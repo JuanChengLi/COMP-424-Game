7b911b53b5c8b38bd707f67a7a9866291d1a6c05
JuanChengLi <junchengli02@gmail.com>
2022-11-26 18:34:04 -0500

    Add files via upload

--- student_agent.py
+++ student_agent.py
@@ -1,4 +1,6 @@
 # Student agent: Add your own agent here
+import math
+
 from agents.agent import Agent
 from store import register_agent
 import sys
@@ -24,16 +26,25 @@ class StudentAgent(Agent):
     def step(self, chess_board, my_pos, adv_pos, max_step):
         # Moves (Up, Right, Down, Left)
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
         # Opposite Directions
         opposites = {0: 2, 1: 3, 2: 0, 3: 1}
+        all_moves = self.findAllMoves(chess_board, my_pos, adv_pos, max_step)
+        my_pos = (all_moves[-1][0], all_moves[-1][1])
+        dir = all_moves[-1][2]
+        print(all_moves)
+
+
+
 
 
 
         # dummy return
         return my_pos, dir
 
-    def findAllMoves(self,chess_board, my_pos, adv_pos, max_step):
+    def distance(self, my_pos, adv_pos):
+        return math.sqrt((my_pos[0] - adv_pos[0])**2 + (my_pos[1] - adv_pos[1])**2)
+
+    def findAllMoves(self, chess_board, my_pos, adv_pos, max_step):
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
         # BFS
         cur_step = 0
@@ -55,25 +66,29 @@ class StudentAgent(Agent):
             cur_pos, cur_step = state_queue.pop(0)
             r, c = cur_pos
             # safe break
-            if cur_step == max_step:
+            if cur_step == max_step or len(state_queue) == 0:
                 break
             # checks all moves u,r,d,l
             # checks all dir for wall
             for move in moves:
                 # next position
-                next_pos = cur_pos + move
+                next_pos = (cur_pos[0] + move[0], cur_pos[1] + move[1])
                 x, y = next_pos
-                for direction in range(4):
-                    # checks if there is wall
-                    if chess_board[r, c, direction]:
-                        continue
+                if x < 0 or x > chess_board.shape[0] or y < 0 or y > chess_board.shape[1]:
+                    for direction in range(4):
+                        # checks if there is wall
+                        if chess_board[x, y, direction]:
+                            continue
 
-                    # skip next position if not valid move or already visited
-                    if next_pos == adv_pos or tuple(next_pos) in visited:
-                        continue
-                    else:
-                        all_moves.append((x, y, direction))
-                # update queue and visited positions
-                visited.add(tuple(next_pos))
-                state_queue.append((next_pos, cur_step + 1))
+                        # skip next position if not valid move or already visited
+                        if next_pos == adv_pos or tuple(next_pos) in visited:
+                            continue
+                        else:
+                            all_moves.append((x, y, direction))
+                    # update queue and visited positions
+                    visited.add(tuple(next_pos))
+                    state_queue.append((next_pos, cur_step + 1))
         return all_moves
+
+    def check_valid_input(self, x, y, dir, x_max, y_max):
+        return 0 <= x < x_max and 0 <= y < y_max and dir in self.dir_map
